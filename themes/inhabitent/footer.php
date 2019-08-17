e929cca7cc4b88e7a94c0d8480ca8fd2ea5578af
T Grewal <tgrewal@Ts-MacBook-Pro.local>
2019-08-17 15:32:48 -0700

    Add Fonts and Flexbox to Footer

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -14,39 +14,46 @@
 
 				<div class="footer-content container">
 
-					<div class="info-hours">					
+					<div class="info-hours">
+						<div class="contact-info">
 
-					<div class="contact-info">
+									<h3><span> </i> </span class="strong">Contact Info</h3>
+									<p><span> <i class="fa fa-envelope"></i> </span>info@inhabitent.com</p>
+									<p><i class="fa fa-phone-square"></i>
+									<a>778-883-6064</a></p>
+									
+									<div> <!-- Start of Div social icons-->
+										<span> <i class="fa fa-facebook-square"></i> </span>
+										<span> <i class="fa fa-twitter-square"></i> </span>
+										<span> <i class="fa fa-youtube-square"></i> </span>
+									</div> <!-- End of Div social icons-->
+									
+							</div> <!-- End of contact-info -->
 
-					<h3><span> </i> </span class="strong">Contact Info</h3>
-					<p><span> <i class="fa fa-envelope"></i> </span>info@inhabitent.com</p>
-					<p><i class="fa fa-phone-square"></i>778-883-6064</p>
+							<div class="business-hours">
+								<h3><span class="strong">Business Hours<span></h3>
+								<p><span class="timeperiod">Monday-Friday:</span>9am to 5pm</p>
+								<p><span class="timeperiod">Saturday:</span>10am to 2pm</p>
+								<p><span class="timeperiod">Sunday:</span>Closed</p>	
+							</div> <!-- End of Div class Business Hours-->					
+
+						</div>	<!-- End of Div class info-hours -->
+
+					<div class="logo-block" >
+						<div class="footer-logo">
+							<a href=""<?php echo get_template_directory_uri();?> rel="home" >
+							<img src="<?php echo get_template_directory_uri();?>/images/logos/inhabitent-logo-text.svg" alt="logo"/>
+							</a>
+						</div>
+					</div> <!-- End of Div class footer-logo-->
+
+				</div> <!-- End of Div class foot content-->
 					
-					<div>
-					<span> <i class="fa fa-facebook-square"></i> </span>
-					<span> <i class="fa fa-twitter-square"></i> </span>
-					<span> <i class="fa fa-youtube-square"></i> </span>
-</div>		
-					</div>
-
-					<div class="business-hours">
-					<h3><span class="strong">Business Hours<span></h3>
-					<p><span>Monday-Friday:</span>9am to 5pm</p>
-					<p><span>Saturday:</span>10am to 2pm</p>
-					<p><span>Sunday:</span>Closed</p>	
-
-					</div>
-
-					</div>	
-
-					<div class="footer-logo">
-						<img src="<?php echo get_template_directory_uri();?>/images/logos/inhabitent-logo-text.svg" alt="logo"/>
-						
-					</div>
-					</div>
+					
 					<div class="copyright-info">
 						<p>COPYRIGHT © 2019 INHABITENT</p>
-					</div>
+					
+					</div> <!-- End of Div class copyright info-->
 				
 					
 				
