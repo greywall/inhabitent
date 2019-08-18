7431035043e8c2506f09d22b12bba5338f025321
T Grewal <tgrewal@Ts-MacBook-Pro.local>
2019-08-18 15:44:40 -0700

    Add Padding and Margins to Find us Page

--- themes/inhabitent/page.php
+++ themes/inhabitent/page.php
@@ -5,7 +5,11 @@
  * @package RED_Starter_Theme
  */
 
-get_header(); ?>
+get_header(); 
+
+get_header();
+get_header();
+?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
