5ced568549f706752c9cf66a5472ad02c93fefec
T Grewal <tgrewal@Ts-MacBook-Pro.local>
2019-08-17 20:14:42 -0700

    Add html content for Header

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -22,26 +22,29 @@
 
 			<header id="masthead" class="site-header" role="banner">
 				<div class="container inner-header">
-					<div class="site-branding">
-						<!-- <h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-						<p class="site-description"><?php bloginfo( 'description' ); ?></p> -->
+						<div class="site-branding">
+							
 
-					<div class="logo">
-						<a href="http://localhost:8888/inhabitent/" rel="home">
-						<h1 class="site-logo">Inhabitent</h1>
-						</a>
-					</div>	
+							<div class="logo">
+								<a href="http://localhost:8888/inhabitent/" rel="home">
+								<h1 class="site-logo">Inhabitent</h1>
+								</a>
+							</div>	
 
-					</div><!-- .site-branding -->
+						</div><!-- .site-branding -->
 
 					<nav id="site-navigation" class="main-navigation" role="navigation">
-						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
-						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+						<div class="main-menu">
+							<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
+							<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+						</div>
+
+						<div class="header-search">
+							<?php get_search_form() ?>
+						</div>
+
 					</nav><!-- #site-navigation -->
-				
-				
-					<?php get_search_form() ?>
-				</div>
+				</div> <!-- #end of Inner Header Container -->
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
