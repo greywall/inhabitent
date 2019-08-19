d3c499ce6f5250d39056932bfd0f272f2f26499d
T Grewal <tgrewal@Ts-MacBook-Pro.local>
2019-08-19 11:25:29 -0700

    Remove Flex for About Page Hero + Add Center Style to About Page Paragraph

--- themes/inhabitent/archive-product.php
+++ themes/inhabitent/archive-product.php
@@ -26,7 +26,7 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php
-					get_template_part( 'template-parts/content' );
+					// get_template_part( 'template-parts/content' );
 
 
 					echo get_the_permalink(); 
