1699fc9a9bc3ce8892c57b85cfc2a728135a8cb1
T Grewal <tgrewal@Ts-MacBook-Pro.local>
2019-08-20 01:15:45 -0700

    Add content for Shop Page

--- themes/inhabitent/archive-product.php
+++ themes/inhabitent/archive-product.php
@@ -10,34 +10,47 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<h2>Shop Stuff</h2>
-		
-
 		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
+		<header class="page-header">
+			<h1>Shop Stuff</h1>
+			<div class="product-description">
 				<?php
-					// the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				$arg = [
+					'taxonomy' => 'product-type',
+					'hide_empty' => false,
+				];
+				$terms = get_terms($arg);
+				foreach($terms as $term){ ?>
+				<p>
+					<a class="tax-desc" href="<?php echo get_term_link($term); ?>"> 
+						<?php echo $term->slug ?>
+					</a>
+				</p>
+				<?php	
+				};
 				?>
+			</div>	<!-- end of product-description -->
 			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-					// get_template_part( 'template-parts/content' );
-
-
-					echo get_the_permalink(); 
-					?> <img class="wp-post-image" src=<?php the_post_thumbnail() ; ?>
-					<?php the_title();
-					
-					
-					echo CFS()->get( 'product_price' );
-
-				?>
-
+			
+			<div class="product-grid">
+				<div class="thumbnail-wrapper">	
+					 <a href="<?php echo get_the_permalink(); ?>">
+						<?php the_post_thumbnail(); ?> 
+					</a>
+				</div>	
+				<div class="product-info">
+					<h2 class="product-title-name">
+					<?php the_title(); ?>					
+					<span class="dots">..............................</span>
+					<span class="product-price">
+					<?php echo CFS()->get( 'product_price' ); ?>
+					</span>	
+					</h2>		
+				</div>
+			</div>	
 			<?php endwhile; ?>
 
 			<?php the_posts_navigation(); ?>
